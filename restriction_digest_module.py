452d516fec7ea5477c114330f7be7216a1a623de
Alexander Wong <wongak626@gmail.com>
2017-06-10 20:03:19 -0400

    Edit to add class objects in module.

--- restriction_digest_module.py
+++ restriction_digest_module.py
@@ -1,84 +1,96 @@
 #import Bio.SeqIO
 
-def TRYPSIN(sequence):
-    cut_sites = [0]
-    for i in range(0,len(sequence)-1):
-        if sequence[i] == 'K' and sequence[i+1] != 'P':
-            cut_sites.append(i+1)
-        elif sequence[i] == 'R' and sequence[i+1] != 'P':
-            cut_sites.append(i+1)
+class digest(object):
 
-    if cut_sites[-1] != len(sequence):
-        cut_sites.append(len(sequence))
-    return cut_sites
-
-def CNBR(sequence):
-	cut_sites = [0]
-	for i in range(0,len(sequence)-1):
-		if sequence[i] == 'M':
-			cut_sites.append(i+1)
+	def __init__(self, sequence, miss_cleavage, min_weight, max_weight, min_length, max_length):
+		self.sequence = str(sequence)
+		self.miss_cleavage = int(miss_cleavage)
+#		self.enzyme = str(enzyme)
+		self.min_weight = int(min_weight)
+		self.max_weight = int(max_weight)
+		self.min_length = int(min_length)
+		self.max_length = int(max_length)
 	
-	if cut_sites[-1] != len(sequence):
-		cut_sites.append(len(sequence))
-	return cut_sites
 	
-def LYSC(sequence):
-	cut_sites = [0]
-	for i in range(0,len(sequence)-1):
-		if sequence[i] == 'K':
-			cut_sites.append(i+1)
-	
-	if cut_sites[-1] != len(sequence):
-		cut_sites.append(len(sequence))
-	return cut_sites
+	def TRYPSIN(self):
+		cut_sites = [0]
+		for i in range(0,len(self.sequence)-1):
+			if self.sequence[i] == 'K' and self.sequence[i+1] != 'P':
+				cut_sites.append(i+1)
+			elif self.sequence[i] == 'R' and self.sequence[i+1] != 'P':
+				cut_sites.append(i+1)
 		
-
-
-def missed_cleavage(sequence,miss_cleavage,cut_sites):
-    peptides = []
-    if len(cut_sites) > 2:
-        # No missed cleavage
-        if miss_cleavage == 0:
-            for j in range(0,len(cut_sites)-1):
-                peptides.append(sequence[cut_sites[j]:cut_sites[j+1]])
-        # 1 missed cleavage
-        elif miss_cleavage == 1:
-            for j in range(0,len(cut_sites)-2):
-                peptides.append(sequence[cut_sites[j]:cut_sites[j+1]])
-                peptides.append(sequence[cut_sites[j]:cut_sites[j+2]])
-            peptides.append(sequence[cut_sites[-2]:cut_sites[-1]])
-
-        # 2 missed cleavages
-        elif miss_cleavage == 2:
-            for j in range(0,len(cut_sites)-3):
-                peptides.append(sequence[cut_sites[j]:cut_sites[j+1]])
-                peptides.append(sequence[cut_sites[j]:cut_sites[j+2]])
-                peptides.append(sequence[cut_sites[j]:cut_sites[j+3]])
-
-            peptides.append(sequence[cut_sites[-3]:cut_sites[-2]])
-            peptides.append(sequence[cut_sites[-3]:cut_sites[-1]])
-            peptides.append(sequence[cut_sites[-2]:cut_sites[-1]])
-    else:
-        peptides.append(sequence)
-    return peptides
-    
-def peptide_dictionary(peptides):
-    AA_weight = {'A':89,'R':174,'N':132,'D':133,'B':133,'C':121,'Q':146,'E':147,'Z':147,'G':75,'H':155,'I':131,'L':131,'K':146,'M':149,'F':165,'P':115,'S':105,'T':119,'W':204,'Y':181,'V':117}
-    peptide_dictionary = {}
-    for peptide in peptides:
-        peptide_weight = 0
-        for letter in peptide:
-            peptide_weight = AA_weight[letter] + peptide_weight
-        #peptide_weights.append(peptide_weight)
-        peptide_dictionary[peptide] = [peptide_weight,len(peptide)]
-    return peptide_dictionary
-
-    
-def read_seq_from_file(seqfilename):
-    sequence = []
-    seqfile = open(seqfilename)
-    for seq_record in Bio.SeqIO.parse(seqfile,"fasta"):
-        sequence.append(seq_record.seq)
-    sequence = str(sequence[0])
-    seqfile.close()
-    return sequence
+		if cut_sites[-1] != len(self.sequence):
+			cut_sites.append(len(self.sequence))
+		return cut_sites
+	
+	def CNBR(self):
+		cut_sites = [0]
+		for i in range(0,len(self.sequence)-1):
+			if self.sequence[i] == 'M':
+				cut_sites.append(i+1)
+		
+		if cut_sites[-1] != len(self.sequence):
+			cut_sites.append(len(self.sequence))
+		return cut_sites
+	
+	def LYSC(self):
+		cut_sites = [0]
+		for i in range(0,len(self.sequence)-1):
+			if self.sequence[i] == 'K':
+				cut_sites.append(i+1)
+		
+		if cut_sites[-1] != len(self.sequence):
+			cut_sites.append(len(self.sequence))
+		return cut_sites
+		
+		
+	def missed_cleavage(self, cut_sites):
+		peptides = []
+		if len(cut_sites) > 2:
+			# No missed cleavage
+			if self.miss_cleavage == 0:
+				for j in range(0, len(cut_sites)-1):
+					peptides.append(self.sequence[cut_sites[j]:cut_sites[j+1]])
+			# 1 missed cleavage
+			elif self.miss_cleavage == 1:
+				for j in range(0,len(cut_sites)-2):
+					peptides.append(self.sequence[cut_sites[j]:cut_sites[j+1]])
+					peptides.append(self.sequence[cut_sites[j]:cut_sites[j+2]])
+				peptides.append(self.sequence[cut_sites[-2]:cut_sites[-1]])
+			
+			# 2 missed cleavages
+			elif self.miss_cleavage == 2:
+				for j in range(0,len(cut_sites)-3):
+					peptides.append(self.sequence[cut_sites[j]:cut_sites[j+1]])
+					peptides.append(self.sequence[cut_sites[j]:cut_sites[j+2]])
+					peptides.append(self.sequence[cut_sites[j]:cut_sites[j+3]])
+				
+				peptides.append(self.sequence[cut_sites[-3]:cut_sites[-2]])
+				peptides.append(self.sequence[cut_sites[-3]:cut_sites[-1]])
+				peptides.append(self.sequence[cut_sites[-2]:cut_sites[-1]])
+		else:
+			peptides.append(self.sequence)
+		return peptides
+	
+	def peptide_dictionary(self, peptides):
+		AA_weight = {'A':89,'R':174,'N':132,'D':133,'B':133,'C':121,'Q':146,'E':147,'Z':147,'G':75,'H':155,'I':131,'L':131,'K':146, 'M':149, 'F':165, 'P':115, 'S':105,'T':119, 'W':204,'Y':181,'V':117}
+		peptide_dictionary = {}
+		for peptide in peptides:
+			peptide_weight = 0
+			for letter in peptide:
+				peptide_weight = AA_weight[letter] + peptide_weight
+			#peptide_weights.append(peptide_weight)
+			peptide_dictionary[peptide] = [peptide_weight, len(peptide)]
+		return peptide_dictionary
+	
+	def peptide_filter(self, peptide_dictionary):
+		result = {}
+		for peptide, (peptide_weight, peptide_length) in peptide_dictionary.iteritems():
+			peptide_weight = float(peptide_weight)
+			peptide_length = float(peptide_length)
+			if self.min_weight <= peptide_weight <= self.max_weight:
+				if self.min_length <= peptide_length <= self.max_length:
+					result[peptide] = [peptide_weight, peptide_length]
+		return result
+	
